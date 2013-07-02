75a6f73b4af3b8df6d0bc362333bb283db548515
Hack Reactor Students <github@hackreactor.com>
2013-07-01 18:27:56 -0700

    chatrooms working, refresh interval reset

--- js/app.js
+++ js/app.js
@@ -1,48 +1,55 @@
 $(document).ready(function(){
 
   var updateMessages = function(lastPulled){
-    lastPulled = lastPulled || '1900-01-01T00:00:00.000Z';
+    var dataString;
+    if (user.activeRoom === null || user.activeRoom === 'the hallway') {
+      dataString = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + user.lastPulled + '"}}}&order=-createdAt&limit=20';
+    } else {
+      dataString = 'where={"createdAt":{"$gt":{"__type":"Date","iso":"' + user.lastPulled + '"}},"roomname":"'+user.activeRoom+'"}&order=-createdAt&limit=20';
+    }
+
     $.ajax({
       type: "GET",
       url: 'https://api.parse.com/1/classes/messages',
-      // data: 'order=-createdAt',
-      // data: 'where={"createdAt":{"$gte":' + lastPulled + '}}',
-      // order=-createdAt&limit=20
-      data: 'where={"createdAt":{"$gte":{"__type":"Date","iso":"' + lastPulled + '"}}}&order=-createdAt&limit=20',
+      // data: 'where={"createdAt":{"$gte":{"__type":"Date","iso":"' + lastPulled + '"}}}&order=-createdAt&limit=20',
+      // data: 'where={"$and":[{"createdAt":{"$gte":{"__type":"Date","iso":"' + lastPulled + '"}}},{"roomname":"' + user.activeRoom + '"}]}&order=-createdAt&limit=20'
+      data: dataString,
       contentType: 'application/json',
       success: function(data){
-        // remove messages if more than 20
-        $('.messageContainer:gt(19)').remove();
-        // add new messages
-        for(var i = data.results.length - 1; i >= 0; i--){
-            data.results[i].username = data.results[i].username || 'unknown';
-            var $tempContainer = $('<div class="messageContainer"> \
-                                      <div class="created" /> \
-                                      <div class="username"><a href="#" class="usernameLink"/></div> \
-                                      <div class="text" /> \
-                                    </div>').prependTo('#main');
-            for (var key in user.friends) {
-              key === data.results[i].username && $tempContainer.addClass('friend');
-            }
-            $tempContainer.find('.created').text(moment(data.results[i].createdAt, "YYYY-MM-DDTHH:mm:ss.SSSZ").fromNow());
-            $tempContainer.find('.usernameLink').text('@' + data.results[i].username);
-            $tempContainer.find('.text').text(data.results[i].text);
+        if (data.results[0]) {
+          // remove messages if more than 20
+          $('.messageContainer:gt(19)').remove();
+          // add new messages
+          for(var i = data.results.length - 1; i >= 0; i--){
+              data.results[i].username = data.results[i].username || 'unknown';
+              var $tempContainer = $('<div class="messageContainer"> \
+                                        <div class="created" /> \
+                                        <div class="username"><a href="#" class="usernameLink"/></div> \
+                                        <div class="text" /> \
+                                      </div>').prependTo('#main');
+              for (var key in user.friends) {
+                key === data.results[i].username && $tempContainer.addClass('friend');
+              }
+              $tempContainer.find('.created').text(moment(data.results[i].createdAt, "YYYY-MM-DDTHH:mm:ss.SSSZ").fromNow());
+              $tempContainer.find('.usernameLink').text('@' + data.results[i].username);
+              $tempContainer.find('.text').text(data.results[i].text);
+          }
+          user.lastPulled = data.results[0].createdAt;
         }
-        lastPulled = data.results[0].createdAt;
         console.log(data);
-        setTimeout(updateMessages, 5000, lastPulled);
+        setTimeout(updateMessages, 1000, lastPulled);
       },
       error: function(data) {
         console.log('Ajax request failed');
       }
     });
-    console.log(lastPulled);
   };
 
   var sendMessage = function(message){
     var newMessage = {
       'username': user.name,
-      'text': message
+      'text': message,
+      'roomname': user.activeRoom
     };
     $.ajax({
       type: "POST",
@@ -50,7 +57,6 @@ $(document).ready(function(){
       contentType: 'application/json',
       data: JSON.stringify(newMessage),
       success: function(){
-        updateMessages();
       },
       error: function(req, err){
         console.log('error: ',err);
@@ -60,7 +66,9 @@ $(document).ready(function(){
   };
 
   var user = {
-    friends: {}
+    friends: {},
+    activeRoom: null,
+    lastPulled: '1900-01-01T00:00:00.000Z'
   };
 
   // check is username is passed in the url, if not promt for it
@@ -72,20 +80,43 @@ $(document).ready(function(){
     user.name = prompt('What is your username?', '') || 'guest';
   }
 
-  $('#username').text(user.name + ': ');
-  updateMessages();
 
   $('#newMessage').on('submit', function(e){
     e.preventDefault();
-    sendMessage($('input').val());
-    $('input').val(null);
+    sendMessage($(this).find('input').val());
+    $(this).find('input').val(null);
+  });
+
+  $('#currentRoom').on('submit', function(e){
+    e.preventDefault();
+    user.activeRoom = $(this).find('input').val();
+    $(this).find('input').attr('placeholder', "You're chatting in " + user.activeRoom).val(null);
+    $('#main').html(null);
+    user.lastPulled = '1900-01-01T00:00:00.000Z';
+  });
+
+  $('#defaultRoom').on('click', function(e){
+    e.preventDefault();
+    user.activeRoom = 'the hallway';
+    $('#currentRoom').find('input').attr('placeholder', "You're chatting in " + user.activeRoom).val(null);
+    $('#main').html(null);
+    user.lastPulled = '1900-01-01T00:00:00.000Z';
   });
 
   $('#main').delegate('.usernameLink', 'click', function(e){
     e.preventDefault();
     user.friends[$(this).text().substring(1)] = true;
-    //console.log(user.friends);
-    console.log(user.friends);
   });
 
+  // $('<div class="loading" />')
+  //   .ajaxStart(function() {
+  //       $(this).prependTo('#main');
+  //   })
+  //   .ajaxStop(function() {
+  //       $(this).remove();
+  //   });
+
+
+  $('#username').text(user.name + ': ');
+  updateMessages();
 });
